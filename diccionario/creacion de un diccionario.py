4b20cd4d59ed971f596f7a290439be46c2bdfb71
AracelyQuinta <quiarabel2006@gmail.com>
2025-03-26 21:50:14 -0500

    utilización de diccionarios

--- diccionario/creacion de un diccionario.py
+++ diccionario/creacion de un diccionario.py
@@ -1,28 +1,28 @@
 # Definición del diccionario con información inicial
-diccionario= {"nombre":"Ana", "edad": 24, "ciudad": "Santa Cecilia", "profesion": "contadora"}
-print(diccionario)
+informacion_personal= {"nombre":"Ana", "edad": 24, "ciudad": "Santa Cecilia", "profesion": "contadora"}
+print(informacion_personal)
 
 print("*"*20) # Separador visual
 
 # Modificación de la clave "ciudad", actualizacion
-diccionario["ciudad"]="Nueva Loja"
-print(diccionario)
+informacion_personal["ciudad"]="Nueva Loja"
+print(informacion_personal)
 print("*"*20) # Separador visual
 
 # Adición de una nueva clave "profesion2" con el valor "Enfermera"
-diccionario["profesion2"]="Enfermera"
-print(diccionario)
+informacion_personal["profesion2"]="Enfermera"
+print(informacion_personal)
 print("*"*20) # Separador visual
 
 # Verificación de si la clave "telefono" existe en el diccionario
-print("telefono" in diccionario)
+print("telefono" in informacion_personal)
 print("*"*20) # Separador visual
 
 # Adición de la clave "telefono" con el valor asignado
-diccionario["telefono"]= "0991478591"
-print(diccionario)
+informacion_personal["telefono"]= "0991478591"
+print(informacion_personal)
 print("*"*20) # Separador visual
 
 # Eliminación de la clave "edad" del diccionario
-del diccionario["edad"]
-print(diccionario)
+del informacion_personal["edad"]
+print(informacion_personal)
